72b612150737943d56adc589187432230a0377ac
Nicolás Vera <nicolasverapalominos@gmail.com>
2018-05-30 18:04:34 -0400

    composer require faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    	$this->call(clientsTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
